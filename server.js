f1b00fce0de603ddb26e2e0172885e53c7a5238e
Christopher Donner <christopher.donner@gmail.com>
2019-10-17 09:18:15 -0400

    garbage

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+// require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
-var db = require("./models");
+// var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3101;
@@ -13,15 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
 
-require("./routes/htmlRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 // mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
 
